f8211f4c22d02776ad21629b8fbada794f279271
rosenclever <rosenclever@gmail.com>
2025-03-27 20:22:16 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaBancario/Model/Conta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.Model
+{
+    public class Conta
+    {
+        public int Numero { get; set; }
+        public double Saldo { get; set; }
+        public Cliente Cliente { get; set; }
+        public Conta(int numero, double saldo, Cliente cliente)
+        {
+            Numero = numero;
+            Saldo = saldo;
+            Cliente = cliente;
+        }
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+        public void Sacar(double valor)
+        {
+            Saldo -= valor;
+        }
+        public void Transferir(double valor, Conta contaDestino)
+        {
+            Sacar(valor);
+            contaDestino.Depositar(valor);
+        }
+    }
+}
---
+++ TestSistemaBancario/TestConta.cs
@@ -0,0 +1,17 @@
+﻿using SistemaBancario.Model;
+
+namespace TestSistemaBancario
+{
+    [TestClass]
+    public sealed class TestConta
+    {
+        [TestMethod]
+        public void TestSaque()
+        {
+            Cliente cliente = new Cliente("Fulano", "123.456.789-00");
+            Conta conta = new Conta(1, 100, cliente);
+            conta.Sacar(50);
+            Assert.AreEqual(50, conta.Saldo);
+        }
+    }
+}
